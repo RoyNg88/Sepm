e847385c63c2ee4e5a66de2019899eeb7e2a7ae8
RoyNg88 <s3741190@rmit.edu.vn>
2021-04-27 04:14:03 +0700

    update

--- Sepm/frontend/src/components/Comment/Comment.js
+++ Sepm/frontend/src/components/Comment/Comment.js
@@ -13,7 +13,57 @@ class Commentt extends Component{
       this.setState({ comment });
     });
   }
+  state = {
+    likes: 0,
+    dislikes: 0,
+    action: null,
+  }
+
+  like = () => {
+    this.setState({
+      likes: 1,
+      dislikes: 0,
+      action: 'liked',
+    });
+  }
+
+  dislike = () => {
+    this.setState({
+      likes: 0,
+      dislikes: 1,
+      action: 'disliked',
+    });
+  }
   render(){
+    const { likes, dislikes, action } = this.state;
+
+    const actions = [
+      <span>
+        <Tooltip title="Like">
+          <Icon
+            type="like"
+            theme={action === 'liked' ? 'filled' : 'outlined'}
+            onClick={this.like}
+          />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: 'auto' }}>
+          {likes}
+        </span>
+      </span>,
+      <span>
+        <Tooltip title="Dislike">
+          <Icon
+            type="dislike"
+            theme={action === 'disliked' ? 'filled' : 'outlined'}
+            onClick={this.dislike}
+          />
+        </Tooltip>
+        <span style={{ paddingLeft: 8, cursor: 'auto' }}>
+          {dislikes}
+        </span>
+      </span>,
+      <span>Reply to</span>,
+    ];
     return(
   <Comment.Group>
     <Header as='h3' dividing>
