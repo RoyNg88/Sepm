2e10c6d560f420cff8c379d062674a145baf372b
RoyNg88 <s3741190@rmit.edu.vn>
2021-04-27 03:46:28 +0700

    update

---
+++ Sepm/frontend/src/App.js
@@ -0,0 +1,23 @@
+﻿import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import 'semantic-ui-css/semantic.min.css';
+
+import Commentt from "./components/Comment/Comment";
+
+//TODO Web Template Studio: Add routes for your new pages here.
+const App = () => {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = { Commentt } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+}
+
+export default App;
---
+++ Sepm/frontend/src/components/Comment/Comment.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react'
+import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import axios from 'axios'
+
+class Commentt extends Component{
+  state = {
+    comment: [],
+  };
+
+  componentDidMount() {
+    axios.get(`https://6085a8f9d14a8700175781be.mockapi.io/comment/Usercomment`).then(res => {
+      const comment = res.data;
+      this.setState({ comment });
+    });
+  }
+  render(){
+    return(
+  <Comment.Group>
+    <Header as='h3' dividing>
+      Comments
+    </Header>
+    <ul>{this.state.comment.map(comment => (
+    <Comment key={comment.id}>
+      <Comment.Avatar src={comment.avatar} />
+      <Comment.Content>
+        <Comment.Author as='a'>{comment.name}</Comment.Author>
+        <Comment.Metadata>
+          <div>{comment.datetime}</div>
+        </Comment.Metadata>
+        <Comment.Text>{comment.content}</Comment.Text>
+        <Comment.Actions>
+          <Comment.Action>Reply</Comment.Action>
+        </Comment.Actions>
+      </Comment.Content>
+    </Comment> 
+    ))}
+    </ul>
+    <Form reply>
+      <Form.TextArea />
+      <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+    </Form>
+  </Comment.Group>
+    );
+}
+}
+
+export default Commentt;
